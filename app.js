03556b443176f1998e13d58d0a81b08683a5f695
Martin Řezníček <martin.reznicek@digiteqautomotive.com>
2020-12-12 15:20:15 +0100

    simplify and send data to res stream with pipe

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-//http.js module
 var http = require('http');
 var fs = require('fs');
 
-//this function will be called when request occurs and it will pass the request and create stream to response
 http.createServer(function(req, res){
-    //first goes head
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
-    var name = 'Martin';
-    //we pull a content from a file (index.html) and manipulate that
-    html = html.replace('{name}', name);
-    res.end(html);
+    fs.createReadStream(__dirname + '/index.html')
+        //send the data to response stream
+        .pipe(res);  
 
 }).listen(1337, '127.0.0.1');
