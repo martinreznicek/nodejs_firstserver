1e9513b5dfb15639e8bf9c3b77f0bfbcf90c5104
Martin Řezníček <martin.reznicek@digiteqautomotive.com>
2020-12-12 15:10:00 +0100

    Add html in response

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //http.js module
 var http = require('http');
+var fs = require('fs');
 
 //this function will be called when request occurs and it will pass the request and create stream to response
 http.createServer(function(req, res){
     //first goes head
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello world\n');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    html = fs.readFileSync(__dirname + '/index.html')
+    res.end(html);
 
 }).listen(1337, '127.0.0.1');
