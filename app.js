2cacbc82477603978b0bbe9295d6dce12b891ec3
Martin Řezníček <martin.reznicek@digiteqautomotive.com>
2020-12-14 17:45:20 +0100

    Add simple routing

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ var obj = require('./data.json')
 
 http.createServer(function(req, res){
     //res.writeHead(200, { 'Content-Type': 'text/html' });
-    //fs.createReadStream(__dirname + '/index.html').pipe(res); 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(obj));
+    
+    if (req.url === '/'){
+        fs.createReadStream(__dirname + '/index.html')
+        .pipe(res); 
+    }
+    else if (req.url === '/api'){   
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(obj));
+    }
+    else{
+        res.writeHead(404);
+        res.end();
+    }
     
 }).listen(1337, '127.0.0.1');
